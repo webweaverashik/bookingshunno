1f82e049f19cd7e489aaee0df89a65b6ce24d15e
webweaverashik <webweaverashik@gmail.com>
2026-08-09 14:48:05 +0600

    Artisan Route Added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/_maintenance/artisan', function () {
     $key = request('key');
