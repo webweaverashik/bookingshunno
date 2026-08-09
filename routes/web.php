a0ae51de133931a3b17b7b79120252777d7f0c31
webweaverashik <webweaverashik@gmail.com>
2026-08-09 14:45:05 +0600

    Artisan Route Added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,41 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/_maintenance/artisan', function () {
+    $key = request('key');
+
+    if (! $key || ! hash_equals(env('ARTISAN_WEB_KEY', ''), $key)) {
+        abort(403);
+    }
+
+    $command = request('command', 'optimize:clear');
+
+    $allowedCommands = [
+        'optimize:clear',
+        'config:clear',
+        'cache:clear',
+        'route:clear',
+        'view:clear',
+        'storage:link',
+        'migrate',
+        'migrate:fresh',
+        'migrate:refresh',
+        'migrate:refresh --seed',
+    ];
+
+    if (! in_array($command, $allowedCommands, true)) {
+        abort(403, 'Command not allowed.');
+    }
+
+    Artisan::call($command, [
+        '--force' => true,
+    ]);
+
+    return response()->json([
+        'success' => true,
+        'command' => $command,
+        'output'  => Artisan::output(),
+    ]);
 });
